bf6dece5cc435ca87daf51cb5c526f7492ac3624
Mike Kunze <kunze@earthlinginteractive.com>
2017-02-21 08:10:32 -0600

    adding output for sanity

--- server.js
+++ server.js
@@ -71,4 +71,6 @@ let server = http.createServer( (req,res) => {
 		res.end("Only POST /pdfify is supported (you did "+req.method+" "+req.url+")");
 	}
 });
-server.listen(port);
+server.listen( port, ()=> {
+	console.log(`pdfification running on ${port}`);
+});
