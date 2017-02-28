74db802fb4f037f9908a1548d423913eb3e4d039
Dan Stevens <stevens@earthit.com>
2017-02-28 17:39:20 -0600

    Attempt at allowing PDFication of arbitrary URLs.

--- server.js
+++ server.js
@@ -55,8 +55,69 @@ function handlePdfifyRequest(req,res) {
 	});
 }
 
+function decodeUriComponentBetter( c ) {
+	return decodeURIComponent(c).replace(/\+/g,' ');
+}
+
+function parseQueryString( qs ) {
+	if( qs == undefined ) return {};
+	if( qs.length > 0 && qs[0] == '?' ) qs = qs.substr(1);
+	if( qs.length == 0 ) return {};
+	let parts = qs.split('&');
+	let data = {};
+	for( let p in parts ) {
+		let kv = parts[p].split('=');
+		if( kv.length == 2 ) {
+			data[decodeUriComponentBetter(kv[0])] = decodeUriComponentBetter(kv[1]);
+		}
+	}
+	return data;
+}
+
+function handlePdfifyGetRequest(req,res) {
+	let m = /^([^\?]*)(?:\?(.*))?$/.exec(req.url);
+	let qsParams = parseQueryString(m[2]);
+	console.log(req.url, m[2], qsParams);
+	let inputUrl = qsParams['uri'];
+	if( inputUrl == undefined ) {
+		return Promise.reject(new Error("No 'uri' given"));
+	}
+	let waitForDomSelector = qsParams['waitForDomSelector'] || 'body';
+
+	let tempBase = "temp/"+randChars(20);
+	let tempOutputFile = tempBase+".pdf";
+	return new Promise( (resolve,reject) => {
+		setTimeout( () => {
+			let msg = "PDFification of <"+inputUrl+"> with selector '"+waitForDomSelector+"' timed out";
+			console.error(msg)
+			reject(new Error(msg));
+		}, 5000);
+		new PDFGenerator().convertFileToPdf(inputUrl, tempOutputFile, {
+			waitForDomSelector
+		}).then( resolve ).catch( reject );
+	}).then( () => {
+		res.writeHead(200, {'Content-Type': 'application/pdf'});
+		return pipeFromFile(tempOutputFile, res).catch( (err) => {
+			console.error("Error while piping "+tempOutputFile+" back to client");
+			console.error(err.stack);
+		});
+	});
+}
+
 let server = http.createServer( (req,res) => {
-	if( req.method == 'POST' && req.url == '/pdfify' ) {
+	let m = /^([^\?]*)(?:\?.*)?$/.exec(req.url);
+	let path = m[1];
+	if( req.method == 'GET' && path == '/pdfify' ) {
+		handlePdfifyGetRequest(req,res).catch( (err) => {
+			console.error("Request to "+req.method+" "+req.url+" failed; sending 500: "+err.stack+"");
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(err.stack);
+		}).then( () => {
+			res.end(); // Make sure!
+		}).catch( (err) => {
+			console.error("Error sending response? "+err.stack);
+		});
+	} else if( req.method == 'POST' && req.url == '/pdfify' ) {
 		handlePdfifyRequest(req,res).catch( (err) => {
 			console.error("Request to "+req.method+" "+req.url+" failed; sending 500: "+err.stack+"");
 			res.writeHead(500, {'Content-Type': 'text/plain'});
